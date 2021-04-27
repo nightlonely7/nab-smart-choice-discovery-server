9f697f36fab9d08b1e7503a9a88c90beafba65ff
nightlonely7 <Speedup79>
2021-04-27 07:40:53 +0700

    Initial commit

---
+++ src/main/java/au/com/nab/smartchoice/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package au.com.nab.smartchoice.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/nab/smartchoice/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.nab.smartchoice.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
